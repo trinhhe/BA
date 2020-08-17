e84bcbda80440fd909354d31c9db3f76c9334853
trinhhe <henry.trinh94@gmail.com>
2020-08-17 06:22:36 +0200

    test

--- scripts/execute_all.sh
+++ scripts/execute_all.sh
@@ -2,7 +2,8 @@
 
 #execute all the graphs
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+DIR=/cluster/home/trinhhe/gapbs
 
 # GRAPHS=("barabasi_albert_9" "erdos_renyi_Ntimes5" "watts_strogatz_24_0.3" "planted_partition_128_0.5_1div4n")
 GRAPHS=("watts_strogatz_24_0.3")
@@ -11,10 +12,10 @@ do
     for file in ${DIR}/../graphs/unweighted/${G}/*
     do
         OUT=${file##*/}
-        ${DIR}/mincut0 -sf "$file" -n1 > ../../outputs/mincut0/unweighted/${G}/${OUT%.graph}.csv
-        ${DIR}/mincut -sf "$file" -n1 > ../../outputs/mincut/unweighted/${G}/${OUT%.graph}.csv
-        ${DIR}/stoer_wagner -sf "$file" -n1 > ../../outputs/stoer/unweighted/${G}/${OUT%.graph}.csv
-        ${DIR}/karger_stein "$file" 1 > ../../outputs/karger/unweighted/${G}/${OUT%.graph}.csv
+        ${DIR}/mincut0 -sf "$file" -n1 > ../outputs/mincut0/unweighted/${G}/${OUT%.graph}.csv
+        ${DIR}/mincut -sf "$file" -n1 > ../outputs/mincut/unweighted/${G}/${OUT%.graph}.csv
+        ${DIR}/stoer_wagner -sf "$file" -n1 > ../outputs/stoer/unweighted/${G}/${OUT%.graph}.csv
+        # ${DIR}/karger_stein "$file" 1 > ../outputs/karger/unweighted/${G}/${OUT%.graph}.csv
     done
 done
 
--- scripts/mergecsv.sh
+++ scripts/mergecsv.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 
 # DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# DIR=/cluster/home/trinhhe/outputs
+DIR=/home/henry/Documents/ETH/BA/outputs
 
-# GRAPHS=("barabasi_albert_9" "erdos_renyi_Ntimes5" "watts_strogatz_24_0.3" "random_partition_graph_0.25_1div4n")
-GRAPHS=("barabasi_albert_9")
-# MINCUT=('mincut1' 'mincut2' 'mincut3' 'mincut4')
-MINCUT=("mincut3")
-HEADER="nodes, edges, mincut estimate, packing treshold, \"H size, p, packing time, packing value, packing size\", sample size, trees generator time, cut values of sampled trees, end result vs correct result"     # define header
+GRAPHS=("barabasi_albert_9" "erdos_renyi_Ntimes5" "watts_strogatz_24_0.3" "planted_partition_128_0.5_1div4n")
+# GRAPHS=('watts_strogatz_24_0.3')
+MINCUT=('mincut0' 'mincut' 'stoer' 'karger')
+# MINCUT=('mincut')
+# HEADER="nodes, edges, mincut estimate, packing treshold, \"H size, p, packing time, packing value, packing size\", sample size, trees generator time, cut values of sampled trees, end result vs correct result"     # define header
 # MERGED="ba.csv" 
 
 for mincut in "${MINCUT[@]}";
 do
     for g in "${GRAPHS[@]}";
     do
-        echo $HEADER > ${i%_*:(-1)}.csv
-        for i in `ls -v ../test/out/${mincut}/unweighted/${g}/*.csv`; 
-        do
-            sed "/$HEADER/d" $i >> ${i%_*}.csv
-        done
+        cat ${DIR}/${mincut}/unweighted/${g}/*.csv > ${mincut}_unweighted_${g}.csv
     done
     
 done
 
                     
 
-#TODO MAKE IT WORK PROPERLY
